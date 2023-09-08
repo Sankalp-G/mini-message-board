b55d1a5d8d8747e8627259c80ff16369ad81501b
Sankalp-G <sankalp.gawali@gmail.com>
2023-09-08 19:48:40 +0530

    add /new form post route

--- routes/index.js
+++ routes/index.js
@@ -21,7 +21,13 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/new', (req, res) => {
-  res.render('form')
+  res.render('form');
+});
+
+router.post('/new', (req, res) => {
+  messages.push({text: req.body.text, user: req.body.user, added: new Date()});
+
+  res.redirect('/');
 });
 
 module.exports = router;
